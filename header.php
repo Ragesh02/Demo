a935f215d6e48b4b5d4679fc5b8d6241a0434b23
Ragesh02 <137527716+Ragesh02@users.noreply.github.com>
2023-06-24 10:08:28 +0530

    header.php

--- header.php
+++ header.php
@@ -8,5 +8,6 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   </head>
   <body>
