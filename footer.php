2e8df330afc007b1a79ac0bc46804a056516058c
Ragesh <137527716+Ragesh02@users.noreply.github.com>
2023-10-04 16:25:42 +0530

    footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
