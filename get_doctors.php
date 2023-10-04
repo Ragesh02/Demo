34c76b31be2b176fd9d19e6080008e787ede2840
Ragesh <137527716+Ragesh02@users.noreply.github.com>
2023-10-04 16:36:37 +0530

    get_doctors.php

--- get_doctors.php
+++ get_doctors.php
@@ -1,12 +1,11 @@
 <?php
 
-    require_once "db_conn.php";
+    include "db_conn.php";
 
     $dep_id = $_POST['dep_id'];
     $query = "SELECT * FROM `doctors` where doc_dep_id=$dep_id";
     $result = mysqli_query($connect, $query);
     
-    //echo "<select>";
     echo "<option value=''>-Select Doctor-</option>";
     while($row = mysqli_fetch_assoc($result)){
         $option .= "<option value='$row[doc_id]'>" . $row['doc_name'] . "</option>";
