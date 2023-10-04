86d0c03834f4d5afebfdc63c65191018bb0b0f3c
Ragesh <137527716+Ragesh02@users.noreply.github.com>
2023-10-04 16:31:37 +0530

    delete_patient.php

--- delete_patient.php
+++ delete_patient.php
@@ -1,12 +1,12 @@
 <?php 
 
-    require_once "db_conn.php";
+    include "db_conn.php";
 
     if(isset($_GET['pat_id'])){
         $pat_id = $_GET['pat_id'];
     }
 
-    $query = "DELETE FROM `patients` WHERE pat_id = $pat_id";
+    $query = "DELETE FROM patients WHERE pat_id = $pat_id";
     
     $result = mysqli_query($connect, $query);
 
